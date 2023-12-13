d25c1afddff3e297ce15fcec1f61a1dde444730b
rahul more <rahulmore@rahuls-MacBook-Air.local>
2023-12-13 11:12:50 +0530

    [Deploy:Initial deployment]

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { textContainer, textVariant2 } from '../utils/motion';
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -10,7 +10,7 @@ export const TypingText = ({ title, textStyles }) => (
   >
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {letter === ' ' ? '\u00A0' : letter}
+        {letter === " " ? "\u00A0" : letter}
       </motion.span>
     ))}
   </motion.p>
@@ -19,8 +19,8 @@ export const TypingText = ({ title, textStyles }) => (
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
